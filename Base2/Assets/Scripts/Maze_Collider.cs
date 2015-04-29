5e3244966a532316287d1e5f453ac59f49a9b78a
Carakai <lilli.luna@hotmail.de>
2015-04-29 12:13:07 +0200

    Maze_Collider_Level_8

--- Base2/Assets/Scripts/Maze_Collider.cs
+++ Base2/Assets/Scripts/Maze_Collider.cs
@@ -2,25 +2,46 @@
 using System.Collections;
 
 public class Maze_Collider : MonoBehaviour {
+	
+	//public Vector3 angle;
+	private float angle;
+	private float playerViewAngle;
+	private float angleDifference;
 
-	public Vector3 angle;
-	public Player player;
-
+	public Camera cam;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frames
 	void Update () {
-	
 	}
+	
+	void OnTriggerStay(Collider other) {
+		if (other.tag == "Player") {
+			//	Debug.Log ("Player detected");
+			
+			//Quaternion wantedAngle = Quaternion.Euler(angle);
+			
+			angle = transform.rotation.y;
+			playerViewAngle = cam.transform.rotation.y;
 
-	void OnCollisionStay(Collision other) {
-		if (other.gameObject.tag == "Player") {
-			Debug.Log ("Player detected");
+			angleDifference = angle - playerViewAngle;
 
+			if (angleDifference < -0.2) {
+				Debug.Log("Du musst weiter nach links.");
+			} else if (angleDifference > 0.2) { 
+				Debug.Log("Du musst weiter nach rechts.");
+			} else {
+				Debug.Log("Genau richtig!");
+			}
+			//Debug.Log("View: " + playerViewAngle);
+			//Debug.Log("Collider: " + angle);
+			//Debug.Log("Angledifference: " + angleDifference);
+			
 		}
-		Debug.Log ("OnTriggerEnter");
+		//	Debug.Log ("OnTriggerStay");
 	}
 }
