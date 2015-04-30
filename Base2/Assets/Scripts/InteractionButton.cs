5d3323ead813b883be8d85f0a66da8bb04bf7e95
Carakai <lilli.luna@hotmail.de>
2015-04-30 13:58:49 +0200

    Button Interaction + Camera RayCast

---
+++ Base2/Assets/Scripts/InteractionButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionButton : MonoBehaviour {
+	
+	private string loadPrompt;
+	private bool pressButton;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	public void Update () {
+		if (Input.GetButtonDown("ButtonPressed")) 
+		{
+	//		Application.LoadLevel ("Button pressed.");
+			Debug.Log("Button pressed.");
+		}
+	}
+
+	public void OnTriggerStay(Collider other) {
+			loadPrompt = "[F] for Interaction";
+	}
+
+	public void OnTriggerExit() {
+		loadPrompt = "";
+	}
+}
