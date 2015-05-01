9db4e89e656a4a56b88c65cedb6e5992c651c6ad
Floppi003 <f.peinsold@gmail.com>
2015-05-01 10:56:55 +0200

    some improvements to Level 1, fixed bug in Main Menu

--- Base2/Assets/Scripts/Level1/Level1.cs
+++ Base2/Assets/Scripts/Level1/Level1.cs
@@ -5,23 +5,61 @@ using System.Collections;
 
 public class Level1 : MonoBehaviour { 
 
+
 	private float elapsedTime;
 	private bool openFirstDoorAnimationStarted;
+	private bool wallTriggerStarted;
+	private bool wallSoundPlayed;
+
+	private float timeTillFirstDoorOpens;
+	private float timeTillWallSoundStarts;
+
 
 	void Awake() {
+		timeTillFirstDoorOpens = 9.0f;
+		timeTillWallSoundStarts = 3.0f;
+
 		openFirstDoorAnimationStarted = false;
+		wallTriggerStarted = false;
+		wallSoundPlayed = false;
+		Debug.Log ("Awake");
 	}
 
-	void Start() {}
+	void Start() {
+		Debug.Log ("Start");
+	}
 
 	void Update() {
 		elapsedTime += Time.deltaTime;
 
-		if (elapsedTime > 9.0f && !openFirstDoorAnimationStarted) {
-			Debug.Log ("animation started");
+		// If the wall trigger was triggered then start the countdown till the voice should start
+		if (wallTriggerStarted) {
+			timeTillWallSoundStarts -= Time.deltaTime;
+
+			if (wallSoundPlayed == false) {
+				// play Audio Sound
+				AudioManager.instance.queueAudioClip(null);
+			}
+		}
+
+		if (elapsedTime > timeTillFirstDoorOpens && !openFirstDoorAnimationStarted) {
 			openFirstDoorAnimationStarted = true;
 			GameObject.Find ("FirstDoor").GetComponent<Animator>().Play ("OpenDoor");
 		}
 	}
 
+
+	// Triggers
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("OnTriggerEnter");
+		wallTriggerStarted = true;
+	}
+	
+	void OnTriggerStay(Collider other) {
+		Debug.Log ("OnTriggerStay");
+	}
+	
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("OnTriggerExit");
+	}
 }
